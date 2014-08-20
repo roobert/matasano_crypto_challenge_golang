93987b3d854af8e59a6347f12ed9707a9cea0118
Rob Wilson <roobert@gmail.com>
2014-08-20 21:22:05 +0100

    Q2 complete

--- matasano.go
+++ matasano.go
@@ -13,3 +13,16 @@ func HexToBase64(hex_input string) string {
 
 	return base64_output
 }
+
+func XOR(bytes_a, bytes_b []byte) []byte {
+
+	var xor_result []byte
+
+	for i, b := range bytes_a {
+		xored_byte := b ^ bytes_b[i]
+
+		xor_result = append(xor_result, xored_byte)
+	}
+
+	return xor_result
+}
--- matasano_test.go
+++ matasano_test.go
@@ -1,6 +1,9 @@
 package matasano
 
-import "testing"
+import (
+	"encoding/hex"
+	"testing"
+)
 
 func TestHexToBase64(t *testing.T) {
 	hex_input := "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
@@ -10,5 +13,22 @@ func TestHexToBase64(t *testing.T) {
 
 	if base64_output != expected_base64_output {
 		t.Error("base64_output does not match expected output!")
+	}
+}
+
+func TestXOR(t *testing.T) {
+	hex_input_a := "1c0111001f010100061a024b53535009181c"
+	hex_input_b := "686974207468652062756c6c277320657965"
+
+	hex_expected_xor_result := "746865206b696420646f6e277420706c6179"
+
+	bytes_a, _ := hex.DecodeString(hex_input_a)
+	bytes_b, _ := hex.DecodeString(hex_input_b)
+
+	xor_result := XOR(bytes_a, bytes_b)
+	hex_xor_result := hex.EncodeToString(xor_result)
+
+	if hex_xor_result != hex_expected_xor_result {
+		t.Error("xor_result does not match expected_xor_result!")
 	}
 }
